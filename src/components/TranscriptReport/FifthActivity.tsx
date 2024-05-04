054c5f78829e51fbf80a7345bc041b8583910981
Pleng <pacharaprompang1@gmail.com>
2024-05-04 18:32:09 +0700

    Add mock data to FourthActivity and FifthActivity components

--- src/components/TranscriptReport/FifthActivity.tsx
+++ src/components/TranscriptReport/FifthActivity.tsx
@@ -1,6 +1,26 @@
 import { ActivityProps } from "../../types/TranscriptReport.types";
+import { Button, Tab} from "./Button";
 
 export default function FifthActivity({}: ActivityProps) {
+  const test_arr = [
+    {
+      "activityId": "166222222222",
+      "outstanding": "ฟุตบอล"
+    },
+    {
+      "activityId": "166222222222",
+      "outstanding": "ฟุตบอล"
+    },
+    {
+      "activityId": "166222222222",
+      "outstanding": "ฟุตบอล"
+    },
+    {
+      "activityId": "166222222222",
+      "outstanding": "ฟุตบอล"
+    },
+  ]
+
   return (
     <div className="activity-container">
       <div className="activity-header">
@@ -8,7 +28,21 @@ export default function FifthActivity({}: ActivityProps) {
           ได้รับคัดเลือกให้เป็นนิิสิตดีเด่นตามประกาศของมหาวิทยาลัย
         </p>
       </div>
-      <button className="activity-details-button">ทั้งหมด</button>
+      <div className="activity-lists">
+        {
+          test_arr.map((activity, index) => (
+            <div className="activity" key={index}>
+              <p className="activity-name">{activity.activityId}</p>
+              <p>{activity.outstanding}</p>
+            </div>
+          ))
+        }
+      </div>
+      <Button
+        handleClick={() => {
+          Tab("https://www.youtube.com/");
+        }}
+      />
     </div>
   );
 }
--- src/components/TranscriptReport/FourthActivity.tsx
+++ src/components/TranscriptReport/FourthActivity.tsx
@@ -2,12 +2,41 @@ import { ActivityProps } from "../../types/TranscriptReport.types";
 import { Button, Tab } from "./Button";
 
 export default function FourthActivity({}: ActivityProps) {
+    const test_arr = [
+        {
+            "activityId": "166111111111",
+            "position": "รองประธาน"
+        },
+        {
+            "activityId": "166111111111",
+            "position": "รองประธาน"
+        },
+        {
+            "activityId": "166111111111",
+            "position": "รองประธาน"
+        },
+        {
+            "activityId": "166111111111",
+            "position": "รองประธาน"
+        },
+      ]
+    
   return (
     <div className="activity-container">
       <div className="activity-header">
         <p className="activity-header-title">
           เข้าร่วมกิจกรรมในฐานะกรรมการบริหารองค์กรกิจกรรม
         </p>
+      </div>
+      <div className="activity-lists">
+        {
+          test_arr.map((activity, index) => (
+            <div className="activity" key={index}>
+              <p className="activity-name">{activity.activityId}</p>
+              <p>{activity.position}</p>
+            </div>
+          ))
+        }
       </div>
       <Button
         handleClick={() => {
