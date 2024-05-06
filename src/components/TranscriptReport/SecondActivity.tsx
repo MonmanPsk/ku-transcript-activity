02746084984577833c0ee7e4d68e991256eaa1b7
MonmanPsk <monman321@hotmail.com>
2024-05-06 16:07:31 +0700

    Complete SecondActivity Format

--- src/components/TranscriptReport/SecondActivity.tsx
+++ src/components/TranscriptReport/SecondActivity.tsx
@@ -1,6 +1,17 @@
 import { ActivityProps } from "../../types/TranscriptReport.types";
 
-export default function SecondActivity({}: ActivityProps) {
+export default function SecondActivity({ }: ActivityProps) {
+  const test_arr = [
+    {
+      "activityName": "กิจกรรมหนึ่ง",
+      "hours": 4
+    },
+    {
+      "activityName": "กิจกรรมสอง",
+      "hours": 2
+    },
+  ]
+
   return (
     <div className="activity-container">
       <div className="activity-header">
@@ -21,7 +32,61 @@ export default function SecondActivity({}: ActivityProps) {
           <div className="activity-progress-frontbar"></div>
         </div>
       </div>
-      <div className="activity-lists">{}</div>
+      <div className="activity-lists">
+        <div className="activity-sub-lists-container">
+          <div className="activity-sub-lists-header">
+            <p className="activity-sub-lists-header-title">ด้านพัฒนาคุณธรรมจริยธรรม</p>
+            <p >(ไม่น้อยกว่า 1 กิจกรรม)</p>
+          </div>
+          <div className="activity-sub-lists">
+            -
+          </div>
+        </div>
+        <div className="activity-lists-divider"></div>
+        <div className="activity-sub-lists-container">
+          <div className="activity-sub-lists-header">
+            <p className="activity-sub-lists-header-title">ด้านพัฒนาทักษะการคิดและการเรียนรู้</p>
+            <p >(ไม่น้อยกว่า 1 กิจกรรม)</p>
+          </div>
+          <div className="activity-sub-lists">
+            {
+              test_arr.map((activity, index) => (
+                <div className="activity" key={index}>
+                  <p className="activity-name">{activity.activityName}</p>
+                  <p>{activity.hours}</p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className="activity-lists-divider"></div>
+        <div className="activity-sub-lists-container">
+          <div className="activity-sub-lists-header">
+            <p className="activity-sub-lists-header-title">ด้านพัฒนาทักษะเสริมสร้างความสัมพันธ์ระหว่างบุคคลและการสื่อสาร</p>
+            <p >(ไม่น้อยกว่า 1 กิจกรรม)</p>
+          </div>
+          <div className="activity-sub-lists">
+            {
+              test_arr.map((activity, index) => (
+                <div className="activity" key={index}>
+                  <p className="activity-name">{activity.activityName}</p>
+                  <p>{activity.hours}</p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className="activity-lists-divider"></div>
+        <div className="activity-sub-lists-container">
+          <div className="activity-sub-lists-header">
+            <p className="activity-sub-lists-header-title">ด้านพัฒนาสุขภาพ</p>
+            <p >(ไม่น้อยกว่า 1 กิจกรรม)</p>
+          </div>
+          <div className="activity-sub-lists">
+            -
+          </div>
+        </div>
+      </div>
       <button className="activity-details-button">ทั้งหมด</button>
     </div>
   );
