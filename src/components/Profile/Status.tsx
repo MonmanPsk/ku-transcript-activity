29c8c2afa9295e754f235685abcea4731af0e20b
Pleng <pacharaprompang1@gmail.com>
2024-05-09 09:16:40 +0700

    revise the Status component

--- src/components/Profile/Status.tsx
+++ src/components/Profile/Status.tsx
@@ -2,24 +2,12 @@ export type StatusProps = {
   pass: boolean;
 };
 
-export default function Status(status: StatusProps) {
-  if (status.pass) {
-    //pass the criteria
-    return (
-      <div className="page1-pass-status">
-        <span className="page1-pass-text006">
-          <span>สถานะ : pass</span>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      // not pass the criteria
-      <div className="page1-not-pass-status">
-        <span className="page1-not-pass-text006">
-          <span>สถานะ : not pass</span>
-        </span>
-      </div>
-    );
-  }
+export default function Status({ pass }: StatusProps) {
+  return (
+    <div className={pass ? 'page1-pass-status' : 'page1-not-pass-status'}>
+      <span className={pass ? 'page1-pass-text006' : 'page1-not-pass-text006'}>
+        {pass ? 'สถานะ : pass' : 'สถานะ : not pass'}
+      </span>
+    </div>
+  );
 }
