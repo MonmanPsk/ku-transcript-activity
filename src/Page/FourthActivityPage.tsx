3d4c76463aca1835f76320354104de5eb99a3073
MonmanPsk <monman321@hotmail.com>
2024-05-30 00:41:14 +0700

    Remake Activity Page keep it in the same format, correctly and better responsive

--- src/Page/FourthActivityPage.tsx
+++ src/Page/FourthActivityPage.tsx
@@ -1,66 +1,54 @@
-import { ActivityProps } from "../types/TranscriptReport.types";
-import BackButton from "../components/BackButton";
 import { useLocation } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import { ActivityProps } from "../types/TranscriptReport.types";
 
-const FourthActivityPage = () => {
+import "../styles/activityPage.css";
+
+function FourthActivityPage() {
   const location = useLocation();
   const activities: ActivityProps[][] = location.state;
 
   return (
-    <>
+    <div className="activity-page-container">
       <div className="back-button-container">
         <BackButton path="/" />
       </div>
-      <div className="activity-page-header">
-        <div className="activity-page-header-text">
-          เข้าร่วมกิจกรรมในฐานะกรรมการบริหารองค์กรกิจกรรม
-        </div>
-        <div>
-          <div className="activity-page-table-headbar">
-            <div className="first-column-header">รหัสกิจกรรมโครงการ</div>
-            <div className="second-column-header">หน่วยงาน</div>
-            <div className="third-column-header">ตำแหน่ง</div>
-            <div className="fourth-column-header">หน่วยชั่วโมง</div>
-          </div>
-          <div className="activity-page-table-content">
-            <ul>
-              <li className="activity-table-list">
-                {activities.length > 0 ? (
-                  activities.map((innerArray, index) => (
-                    <div key={index} className="inner-activity-list">
-                      {innerArray.map((activity, innerIndex) => (
-                        <div
-                          className="activity-page-activity"
-                          key={innerIndex}
-                        >
-                          <p className="activity-page-activity-id">
-                            {activity.activityId ? activity.activityId : "-"}
-                          </p>
-                          <p className="activity-page-activity-organization">
-                            {activity.organization
-                              ? activity.organization
-                              : "-"}
-                          </p>
-                          <p className="activity-page-activity-position">
-                            {activity.position ? activity.position : "-"}
-                          </p>
-                          <p className="activity-page-activity-hours">
-                            {activity.hours ? activity.hours : "-"}
-                          </p>
-                        </div>
-                      ))}
-                    </div>
-                  ))
-                ) : (
-                  <div className="no-data-text">-</div>
-                )}
-              </li>
-            </ul>
-          </div>
+      <div className="activity-table-title">
+        <div className="activity-table-title-name">
+          <p>เข้าร่วมกิจกรรมในฐานะกรรมการบริหารองค์กรกิจกรรม</p>
         </div>
       </div>
-    </>
+      <div className="activity-table">
+        <div className="activity-table-header activity-table-row">
+          <div><p>รหัสกิจกรรม</p></div>
+          <div><p>ตำแหน่งบริหาร</p></div>
+        </div>
+        <div className="activity-table-data">
+          {activities.length > 0 ? (
+            activities.map(
+              (activity) => (
+                activity.map((subActivity, innerIndex) => (
+                  <div key={innerIndex} className="activity-table-row">
+                    <div>
+                      <p>{subActivity.activityId}</p>
+                    </div>
+                    <div>
+                      <p>{subActivity.position}</p>
+                    </div>
+                  </div>
+                ))
+              )
+            )
+          ) : (
+            <div className="activity-table-no-data activity-table-row">
+              <div>-</div>
+              <div></div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
-export default FourthActivityPage;
+export default FourthActivityPage
--- src/Page/SecondActivityPage.tsx
+++ src/Page/SecondActivityPage.tsx
@@ -2,56 +2,64 @@ import { useLocation } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import { ActivityProps } from "../types/TranscriptReport.types";
 
+import "../styles/activityPage.css";
+
 function SecondActivityPage() {
     const location = useLocation();
 
     return (
-        <>
+        <div className="activity-page-container">
             <div className="back-button-container">
                 <BackButton path="/" />
             </div>
-            <div className="activity-page-header">
-                <div className="activiity-page-header-text">
-                    กิจกรรมเพื่อเสริมสร้างสมรรถนะ (ไม่น้อยกว่า 4 กิจกรรม)
+            <div className="activity-table-title">
+                <div className="activity-table-title-name">
+                    <p>กิจกรรมเพื่อเสริมสร้างสมรรถนะ&nbsp;</p>
+                    <p>(ไม่น้อยกว่า 4 กิจกรรม)</p>
                 </div>
-                <div>
-                    <div className="activity-page-table-headbar">
-                        <div className="first-column-header">รหัสกิจกรรมโครงการ</div>
-                        <div className="second-column-header">หน่วยงาน</div>
-                        <div className="third-column-header">ตำแหน่ง</div>
-                        <div className="fourth-column-header">หน่วยชั่วโมง</div>
-                    </div>
-                    <div className="activity-page-table-content">
-                        <ul>
-                            <li className="activity-table-list">
-                                {location.state.length > 0 ? (
-                                    location.state.map(
-                                        (activity: ActivityProps, index: number) => (
-                                            <div className="activity-page-activity" key={index}>
-                                                <p className="activity-page-activity-id">
-                                                    {activity.activityId}
-                                                </p>
-                                                <p className="activity-page-activity-organization">
-                                                    {activity.organization}
-                                                </p>
-                                                <p className="activity-page-activity-position">
-                                                    {activity.position}
-                                                </p>
-                                                <p className="activity-page-activity-hours">
-                                                    {activity.hours}
-                                                </p>
-                                            </div>
-                                        )
-                                    )
-                                ) : (
-                                    <div className="no-data-text">-</div>
-                                )}
-                            </li>
-                        </ul>
-                    </div>
+                <div className="activity-table-title-status">
+
                 </div>
             </div>
-        </>
+            <div className="activity-table">
+                <div className="activity-table-header activity-table-row">
+                    <div><p>รหัสกิจกรรม</p></div>
+                    <div><p>โครงการ</p></div>
+                    <div><p>หน่วยงาน</p></div>
+                    <div><p>ตำแหน่ง</p></div>
+                    <div><p>หน่วยชั่วโมง</p></div>
+                </div>
+                <div className="activity-table-data">
+                    {location.state.length > 0 ? (
+                        location.state.map(
+                            (activity: ActivityProps, index: number) => (
+                                <div className="activity-table-row" key={index}>
+                                    <div>
+                                        <p>{activity.activityId}</p>
+                                    </div>
+                                    <div>
+                                        <p>{activity.activityName}</p>
+                                    </div>
+                                    <div>
+                                        <p>{activity.organization}</p>
+                                    </div>
+                                    <div>
+                                        <p>{activity.position}</p>
+                                    </div>
+                                    <div>
+                                        <p>{activity.hours}</p>
+                                    </div>
+                                </div>
+                            )
+                        )
+                    ) : (
+                        <div className="activity-table-no-data activity-table-row">
+                            <div>-</div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
     );
 }
 
